54fda7cd2b2a89f3683192aa836bb374e2dc8411
Daniel Scalzi <d_scalzi@yahoo.com>
2024-08-05 00:55:43 -0400

    Revert back to old version comparison logic #9

--- lib/common/util/MojangUtils.ts
+++ lib/common/util/MojangUtils.ts
@@ -1,5 +1,4 @@
 import { Rule, Natives } from '../../dl/mojang/MojangTypes'
-import semver from 'semver'
 
 export function getMojangOS(): string {
     const opSys = process.platform
@@ -49,5 +48,13 @@ export function isLibraryCompatible(rules?: Rule[], natives?: Natives): boolean 
  * @param {string} actual The actual version.
  */
 export function mcVersionAtLeast(desired: string, actual: string): boolean {
-    return semver.satisfies(actual, `>=${desired}`)
+    const des = desired.split('.')
+    const act = actual.split('.')
+
+    for(let i=0; i<des.length; i++){
+        if(!(parseInt(act[i]) >= parseInt(des[i]))){
+            return false
+        }
+    }
+    return true
 }
---
+++ test/common/util/MojangUtilTest.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai'
+import { mcVersionAtLeast } from '../../../lib/common/util/MojangUtils'
+
+describe('MojangUtil', () => {
+
+    it('mcVersionAtLeast', async () => {
+
+        expect(mcVersionAtLeast('1.21', '1.21')).to.be.true
+        expect(mcVersionAtLeast('1.20.5', '1.20.3')).to.be.false
+        expect(mcVersionAtLeast('1.20.5', '1.20')).to.be.false
+        expect(mcVersionAtLeast('1.20', '1.20.5')).to.be.true
+
+        expect(mcVersionAtLeast('1.12', '1.20.5')).to.be.true
+        expect(mcVersionAtLeast('1.12', '1.12.1')).to.be.true
+        expect(mcVersionAtLeast('1.12', '1.11.2')).to.be.false
+        expect(mcVersionAtLeast('1.12', '1.7')).to.be.false
+
+    })
+
+})
